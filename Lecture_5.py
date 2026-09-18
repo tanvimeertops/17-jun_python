5e3c7868397a8c84f0a0325fb7edbf958efa1220
tanvi <tanvimeer.tops@gmail.com>
2026-09-18 09:35:06 +0530

    18 sep

--- Lecture_5.py
+++ Lecture_5.py
@@ -1,1 +1,23 @@
-#this lecture will be conducted at future
+#range(end)
+print(list(range(3)))
+#0 1 2
+print(list(range(10)))
+print(list(range(-5))) #jump 1
+#   0 1 2
+print(list(range(0)))
+# 0 to -1
+print(list(range(1)))
+# 0
+
+#range 1 (start,end)
+print(list(range(1,3)))
+#1 2 
+
+print(list(range(-5,-1)))
+# -5 -4 -3 -2
+
+print(list(range(-4,-10)))
+# -4 -3
+
+print(list(range(5,1)))
+# 5 
